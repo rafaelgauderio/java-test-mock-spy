edbad6605194ff29a37d14478b3191826b9ed74f
Rafael de Luca <deluca1712@gmail.com>
2023-07-01 20:50:41 -0300

    application.properties and initial seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_vegetable (description, price) VALUES ('Alface',2.50);
+INSERT INTO tb_vegetable (description, price) VALUES ('Tomate Gaúcho',4.50);
+INSERT INTO tb_vegetable (description, price) VALUES ('Cebola',3.80);
+INSERT INTO tb_vegetable (description, price) VALUES ('Rabanete',6.40);
