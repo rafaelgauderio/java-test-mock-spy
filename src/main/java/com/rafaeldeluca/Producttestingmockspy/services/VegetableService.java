670ccfbce3b6d39d645603d520ca3a7b9c6cb30d
Rafael de Luca <deluca1712@gmail.com>
2023-07-02 21:50:20 -0300

    Validate Data

--- src/main/java/com/rafaeldeluca/Producttestingmockspy/services/VegetableService.java
+++ src/main/java/com/rafaeldeluca/Producttestingmockspy/services/VegetableService.java
@@ -3,6 +3,9 @@ package com.rafaeldeluca.Producttestingmockspy.services;
 import com.rafaeldeluca.Producttestingmockspy.dto.VegetableDTO;
 import com.rafaeldeluca.Producttestingmockspy.entities.Vegetable;
 import com.rafaeldeluca.Producttestingmockspy.repositories.VegetableRepository;
+import com.rafaeldeluca.Producttestingmockspy.services.exceptions.InvalidDataException;
+import com.rafaeldeluca.Producttestingmockspy.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ public class VegetableService {
 
     @Transactional(readOnly=false)
     public VegetableDTO insert(VegetableDTO dto) {
+        validateVegetableData(dto);
         Vegetable entity = new Vegetable();
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
@@ -24,13 +28,28 @@ public class VegetableService {
 
     @Transactional(readOnly = false)
     public VegetableDTO update (Long id, VegetableDTO dto) {
-        Vegetable entity = new Vegetable();
-        entity = repository.getReferenceById(id);
-        entity.setDescription(dto.getDescription());
-        entity.setPrice(dto.getPrice());
-        entity = repository.save(entity);
 
-        return new VegetableDTO(entity);
+        try {
+            Vegetable entity = new Vegetable();
+            entity = repository.getReferenceById(id);
+            entity.setDescription(dto.getDescription());
+            entity.setPrice(dto.getPrice());
+            entity = repository.save(entity);
+
+            return new VegetableDTO(entity);
+        } catch (EntityNotFoundException error) {
+            throw new ResourceNotFoundException("Resource not found with the id:" + id);
+        }
+
+    }
+
+    protected void validateVegetableData(VegetableDTO dto) {
+        if(dto.getDescription().isBlank()) {
+            throw new InvalidDataException("Field description can not be empty or blank!");
+        }
+        if(dto.getPrice() == null || dto.getPrice() <= 0) {
+            throw new InvalidDataException("Field price have to greater than zero and not null!");
+        }
     }
 
 
