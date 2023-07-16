d1ee470af77689cc721ad95f01e53fe3025fae47
Rafael de Luca <deluca1712@gmail.com>
2023-07-16 19:38:31 -0300

    simuating saving and find a entity from database with mockito

--- src/test/java/com/rafaeldeluca/Producttestingmockspy/services/VegetableServicesTests.java
+++ src/test/java/com/rafaeldeluca/Producttestingmockspy/services/VegetableServicesTests.java
@@ -1,10 +1,17 @@
 package com.rafaeldeluca.Producttestingmockspy.services;
 
+import com.rafaeldeluca.Producttestingmockspy.dto.VegetableDTO;
+import com.rafaeldeluca.Producttestingmockspy.entities.Vegetable;
 import com.rafaeldeluca.Producttestingmockspy.repositories.VegetableRepository;
+import com.rafaeldeluca.Producttestingmockspy.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class) // annotation indica que não é necessário carregar o contexto da API
 public class VegetableServicesTests {
@@ -17,5 +24,32 @@ public class VegetableServicesTests {
     @Mock
     private VegetableRepository repository;
 
+    private Vegetable vegetable;
+    private VegetableDTO vegetableDTO;
+
+    private Long existingId, nonExistingId;
+
+    // agrupar o que for comum a todos os métodos
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        existingId  = 2L;
+        nonExistingId = 10L;
+
+        vegetable = new Vegetable(2L,"Abacaxi Orgânico",8.90);
+        vegetableDTO = new VegetableDTO(vegetable);
+
+        // simular o comportamento de salvar um produto (vegetable)
+        Mockito.when(repository.save(any())).thenReturn(vegetable);
+
+        // simulando buscar um vegetale por Id existente e não existente no banco
+        Mockito.when(repository.getReferenceById(existingId)).thenReturn(vegetable);
+        Mockito.when(repository.getReferenceById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+
+    }
+
+    // simular getReferenceById quando existe quando não existe o Id do vegetable no banco de dados
 
 }
