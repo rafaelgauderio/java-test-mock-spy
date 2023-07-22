516617929d8fe7f73fb2edd9c241edc3de54d61f
Rafael de Luca <deluca1712@gmail.com>
2023-07-22 19:33:24 -0300

    test insert Vegetable with invalid data

--- src/test/java/com/rafaeldeluca/Producttestingmockspy/services/VegetableServicesTests.java
+++ src/test/java/com/rafaeldeluca/Producttestingmockspy/services/VegetableServicesTests.java
@@ -3,6 +3,7 @@ package com.rafaeldeluca.Producttestingmockspy.services;
 import com.rafaeldeluca.Producttestingmockspy.dto.VegetableDTO;
 import com.rafaeldeluca.Producttestingmockspy.entities.Vegetable;
 import com.rafaeldeluca.Producttestingmockspy.repositories.VegetableRepository;
+import com.rafaeldeluca.Producttestingmockspy.services.exceptions.InvalidDataException;
 import com.rafaeldeluca.Producttestingmockspy.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -68,6 +69,42 @@ public class VegetableServicesTests {
         Assertions.assertEquals(result.getPrice(),8.90);
     }
 
+    @Test
+    public void insertShouldReturnInvalidDataExceptionWhenVegetableDescriptionIsBlank () {
+
+        vegetableDTO.setDescription("");
+
+        VegetableService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateVegetableData(vegetableDTO);
+
+        Assertions.assertThrows(InvalidDataException.class, () -> {
+           VegetableDTO result = serviceSpy.insert(vegetableDTO);
+        });
+    }
+
+    @Test
+    public void insertShouldReturnInvalidDataExceptionWhenVegetablePriceIsNegative () {
+        vegetableDTO.setPrice(-9.5);
+
+        VegetableService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateVegetableData(vegetableDTO);
+
+        Assertions.assertThrows(InvalidDataException.class, () -> {
+            VegetableDTO result = serviceSpy.insert(vegetableDTO);
+        });
+    }
+
+    @Test
+    public void insertShouldReturnInvalidDataExceptionWhenVegetablePriceIsZero () {
+        vegetableDTO.setPrice(0.0);
+
+        VegetableService serviceSpy = Mockito.spy(service);
+        Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateVegetableData(vegetableDTO);
+
+        Assertions.assertThrows(InvalidDataException.class, () -> {
+            VegetableDTO result = serviceSpy.insert(vegetableDTO);
+        });
+    }
 
 
 }
